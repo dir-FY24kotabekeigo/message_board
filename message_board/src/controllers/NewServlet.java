eebf2874e0be0dd488237a79ac20503cc46a9df3
dir-FT24kotabekeigo <keigo.kotabe.c443@dirbato.co.jp>
2024-04-18 11:58:55 +0900

    Index/NewServletの修正

--- message_board/src/controllers/NewServlet.java
+++ message_board/src/controllers/NewServlet.java
@@ -3,16 +3,14 @@ package controllers;
 import java.io.IOException;
 
 import javax.persistence.EntityManager;
+import javax.persistence.metamodel.SetAttribute;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.sql.Timestamp;
-
 import models.Message;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -33,31 +31,15 @@ public class NewServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		EntityManager em = DBUtil.createEntityManager();
-		em.getTransaction().begin();
 		
-		Message m = new Message();
-		
-		String title = "taro";
-		m.setTitle(title);
-		
-		String content = "hello";
-		m.setContent(content);
-		
-		Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-		m.setCreated_at(currentTime);
-		m.setUpdated_at(currentTime);
-		
-		em.persist(m);
-		em.getTransaction().commit();
-		
-		response.getWriter().append(Integer.valueOf(m.getId()).toString());
-		
-		em.close();
+	    request.setAttribute("_token", request.getSession().getId());
+	    
+	    request.setAttribute("message", new Message());
+	    
+	    var rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+	    rd.forward(request, response);
 		
 		
-		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 }
