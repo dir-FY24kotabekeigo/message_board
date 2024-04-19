e93bd785cf5e546da24bbe3cee5851cba8e49045
dir-FT24kotabekeigo <keigo.kotabe.c443@dirbato.co.jp>
2024-04-19 12:50:40 +0900

    バリデーションを追加

--- message_board/src/controllers/UpdateServlet.java
+++ message_board/src/controllers/UpdateServlet.java
@@ -2,7 +2,9 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
+import models.MessageValidator;
 import utils.DBUtil;
 
 /**
@@ -50,6 +53,19 @@ public class UpdateServlet extends HttpServlet {
             var currentTime = new Timestamp(System.currentTimeMillis());
             m.setUpdated_at(currentTime); // 更新日時のみ上書き
 
+            List<String> errors = MessageValidator.validate(m);
+            if(errors.size() > 0) {
+                em.close();
+
+                // フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("message", m);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
+                rd.forward(request, response);
+            } else {
+            
             // データベースを更新
             em.getTransaction().begin();
             em.getTransaction().commit();
@@ -64,4 +80,5 @@ public class UpdateServlet extends HttpServlet {
         }
     }
 
+}
 }
